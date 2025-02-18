7838186530ef43c676589d7f2ceb2a10a5edd6ec
Ndiithi <duncanndiithi@yahoo.com>
2025-02-18 14:11:54 +0300

    set up project structure

--- src/main/java/app/g_agent/proposal_service/ProposalServiceApplication.java
+++ src/main/java/app/g_agent/proposal_service/ProposalServiceApplication.java
@@ -2,8 +2,12 @@ package app.g_agent.proposal_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableJpaAuditing
 public class ProposalServiceApplication {
 
 	public static void main(String[] args) {

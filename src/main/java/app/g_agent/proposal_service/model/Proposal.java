21fb216189e8456bc53461c28b7412502d550022
Ndiithi <duncanndiithi@yahoo.com>
2025-02-20 22:25:39 +0300

    delete proposal

--- src/main/java/app/g_agent/proposal_service/model/Proposal.java
+++ src/main/java/app/g_agent/proposal_service/model/Proposal.java
@@ -9,6 +9,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
@@ -54,7 +55,7 @@ public class Proposal {
     @Column(name = "contact_id", nullable = false)
     private Long contactId;
 
-    @OneToMany
+    @OneToMany(mappedBy = "proposal", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<ProposalDocument> proposalDocuments = new HashSet<>();
 
     // Getters and Setters
